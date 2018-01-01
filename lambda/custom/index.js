f012989d5714dec042be622b3084b55901844c0c
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-01-01 17:17:03 +0800

    Initial commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,60 @@
+const Alexa = require('alexa-sdk')
+const PubNub = require('pubnub')
+const pubnub = new PubNub({
+	publishKey : '',
+	subscribeKey : ''
+})
+
+const APP_ID = undefined
+
+const SKILL_NAME = 'Pimote'
+const HELP_MESSAGE = 'You can control your devices... What can I help you with?'
+const HELP_REPROMPT = 'What can I help you with?'
+const STOP_MESSAGE = 'Goodbye!'
+
+exports.handler = function (event, context, callback) {
+	const alexa = Alexa.handler(event, context)
+	alexa.appId = APP_ID
+	alexa.registerHandlers(handlers)
+	alexa.execute()
+}
+
+const handlers = {
+	'LaunchRequest': function () {
+		this.emit('TurnOnTVIntent')
+	},
+
+	'TurnOnTVIntent': function () {
+		pubnub.publish({
+			channel: 'hello_world',
+			message: {
+				device: 'TV',
+				action: 'power',
+			}
+		}, (status, response) => {
+			console.log('PUBNUB', status, response)
+
+			this.response.cardRenderer(SKILL_NAME, JSON.stringify(response))
+			this.response.speak('OK')
+			this.emit(':responseReady')
+		})
+	},
+
+	'AMAZON.HelpIntent': function () {
+		const speechOutput = HELP_MESSAGE
+		const reprompt = HELP_REPROMPT
+
+		this.response.speak(speechOutput).listen(reprompt)
+		this.emit(':responseReady')
+	},
+
+	'AMAZON.CancelIntent': function () {
+		this.response.speak(STOP_MESSAGE)
+		this.emit(':responseReady')
+	},
+
+	'AMAZON.StopIntent': function () {
+		this.response.speak(STOP_MESSAGE)
+		this.emit(':responseReady')
+	},
+}
